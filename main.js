8b22ade0225a716d3bf99ae5e9b3677ea8b844ad
Vladyslav Hliuzitskyi <115615420+gluzik@users.noreply.github.com>
2023-08-31 17:55:06 +0200

    Додана перевірка, чи користувач ввів число

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
 let num1 = +prompt("Введіть перше число:");
-let operator = prompt("Яку дію Ви хочете виконати: +, -, *, /?");
-let num2 = +prompt("Введіть друге число:");
 
-switch (operator) {
-    case "+":
-        alert( `Сума Ваших введених чисел = ${num1 + num2}`);
-        break;
+if (isNaN(num1)) {
+    alert("Введено некоректне число");
+} else {
+    let operator = prompt("Яку дію Ви хочете виконати: +, -, *, /?");
 
-    case "-":
-        alert( `Різниця Ваших введених чисел = ${num1 - num2}`);
-        break;
+    let num2 = +prompt("Введіть друге число:");
 
-    case "*":
-        alert( `Добуток Ваших введених чисел = ${num1 * num2}`);
-        break;
+    if (isNaN(num2)) {
+        alert("Введено некоректне число");
+    } else {
+        switch (operator) {
+            case "+":
+                alert(`Сума Ваших введених чисел = ${num1 + num2}`);
+                break;
 
-    case "/":
-        if(num2 <= 0){
-            alert("На нуль ділити не можна!");
+            case "-":
+                alert(`Різниця Ваших введених чисел = ${num1 - num2}`);
+                break;
+
+            case "*":
+                alert(`Добуток Ваших введених чисел = ${num1 * num2}`);
+                break;
+
+            case "/":
+                if (num2 <= 0) {
+                    alert("На нуль ділити не можна!");
+                }
+                else {
+                    alert(`Частка Ваших введених чисел = ${num1 / num2}`);
+                }
+                break;
+
+            default:
+                alert("Введено не коректне значення");
+                break;
         }
-        else{
-            alert( `Частка Ваших введених чисел = ${num1 / num2}`);
-        }
-        break;
-
-    default:
-        alert("Введено не коректне значення");
-        break;
+    }
 }
+
+
+
